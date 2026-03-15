0c60d9b4e736ee2b62be7ce7fa3a46ca5065d32f
DIRCEULH <dirceulh@hotmail.com>
2026-03-15 16:25:28 -0300

    Tela de Login - cadastrar

---
+++ app/(tabs)/login/index.tsx
@@ -0,0 +1,62 @@
+// app/(abs)/login/index.tsx
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import axios from 'axios';
+import { useRouter } from 'expo-router';
+import React, { useState } from 'react';
+import { Alert, Button, StyleSheet, TextInput, View } from 'react-native';
+
+export default function LoginScreen() {
+  const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = async () => {
+  try {
+    const res = await axios.post('http://192.168.0.5:3000/register', { email, password });
+    Alert.alert(res.data.message);
+  } catch (err: any) {
+    Alert.alert('Erro', err.response?.data?.message || err.message);
+  }
+};
+
+const handleLogin = async () => {
+  try {
+    const res = await axios.post('http://192.168.0.5:3000/login', { email, password });
+    Alert.alert(res.data.message);
+
+    router.replace('/');
+  } catch (err: any) {
+    Alert.alert('Erro', err.response?.data?.message || err.message);
+  }
+};
+
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Login</ThemedText>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+        autoCapitalize="none"
+        keyboardType="email-address"
+      />
+      <TextInput
+        placeholder="Senha"
+        value={password}
+        onChangeText={setPassword}
+        style={styles.input}
+        secureTextEntry
+      />
+      <Button title="Cadastrar" onPress={handleRegister} />
+      <View style={{ height: 10 }} />
+      <Button title="Login" onPress={handleLogin} />
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', padding: 20, gap: 12 },
+  input: { borderWidth: 1, borderColor: '#888', borderRadius: 6, padding: 10, marginBottom: 8 },
+});
